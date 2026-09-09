54b3184029522ce39a2b9e2ca1ed6ab28e640760
yocn <yoc.yocn@gmail.com>
2026-09-09 16:01:24 +0900

    mutationを追加した

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,12 +24,51 @@ const typeDefs = `#graphql
   type Query {
     getTodos: [Todo!]!
   }
+
+  type Mutation {
+    addTodo(title: String!): Todo!
+    updateTodo(id: ID!, completed: Boolean!): Todo!
+    deleteTodo(id: ID!): Todo!
+  }
 `;
 
 const resolvers = {
   Query: {
     getTodos: () => todos
-  }
+  },
+  Mutation: {
+    addTodo: (_: unknown, { title }: { title: string }) => {
+      const newTodo = {
+        id: String(todos.length + 1),
+        title,
+        completed: false,
+      };
+      todos.push(newTodo);
+      return newTodo;
+    },
+
+    updateTodo: (
+      _: unknown,
+      { id, completed }: { id: string; completed: boolean }
+    ) => {
+      const todo = todos.find((todo) => todo.id === id);
+      if (!todo) {
+        throw new Error("Todo not found");
+      }
+      todo.completed = completed;
+      return todo;
+    },
+
+    deleteTodo: (_: unknown, { id }: { id: string }) => {
+      const index = todos.findIndex((todo) => todo.id === id);
+      if (index === -1) {
+        throw new Error("Todo not found");
+      }
+      const deletedTodo = todos.splice(index, 1);
+      return deletedTodo[0];
+    },
+
+  },
 };
 
 const server = new ApolloServer({
